727bb1fe8445a72d99dd46fba90f82f9976bc5f5
Wade Gibson <86313939+wadegib@users.noreply.github.com>
2025-03-01 07:55:07 -0600

    Update tomcat-upgrade.sh

--- tomcat-upgrade.sh
+++ tomcat-upgrade.sh
@@ -4,8 +4,8 @@
 
 # Set variables
 tcatold=$(curl http://localhost:8080 | rev | cut -c 20-25 | rev)
-tcatnew=9.0.100
-#tcatnew=$(curl https://dlcdn.apache.org/tomcat/tomcat-9/ | awk 'FNR == 9 {print $5}' | cut -c 8-13)
+#tcatnew=9.0.100
+tcatnew=$(curl https://dlcdn.apache.org/tomcat/tomcat-9/ | awk 'FNR == 9 {print $5}' | cut -c 8-14)
 
 wget -P /tmp https://dlcdn.apache.org/tomcat/tomcat-9/v$tcatnew/bin/apache-tomcat-$tcatnew.tar.gz
 tar xzvf /tmp/apache-tomcat-$tcatnew.tar.gz -C /tmp/
