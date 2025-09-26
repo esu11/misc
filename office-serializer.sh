a3017a559937133dd8456d890be5a27e9c1b036e
aw11 <alex.wyatt@esu11.org>
2025-09-26 14:50:56 -0500

    Update office-serializer.sh

--- office-serializer.sh
+++ office-serializer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Execute via Mosyle script "curl https://raw.githubusercontent.com/esu11/misc/refs/heads/main/serializer.sh | bash ",
+# Execute via Mosyle script "curl https://raw.githubusercontent.com/esu11/misc/refs/heads/main/office-serializer.sh | bash ",
 # either in post-install of Office apps or as Custom Command/Script
 
 curl "https://apps.esu11.org/Office/Serializer2024.pkg" -o /tmp/Serializer2024.pkg
