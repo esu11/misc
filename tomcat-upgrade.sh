5088648d26a401a487f2dd9ffb18f438c32372bf
Wade Gibson <86313939+wadegib@users.noreply.github.com>
2025-11-10 13:20:51 -0600

    Upgrade Tomcat version from 9.0.111 to 9.0.112

--- tomcat-upgrade.sh
+++ tomcat-upgrade.sh
@@ -3,9 +3,9 @@
 # Execute via "sudo curl https://raw.githubusercontent.com/esu11/misc/refs/heads/main/tomcat-upgrade.sh | sudo bash"
 
 # Set variables
-tcatold=9.0.110
+tcatold=9.0.111
 #tcatold=$(curl http://localhost:8080 | rev | cut -c 20-25 | rev)
-tcatnew=9.0.111
+tcatnew=9.0.112
 #tcatnew=$(curl https://dlcdn.apache.org/tomcat/tomcat-9/ | awk 'FNR == 9 {print $5}' | cut -c 8-14)
 
 wget -P /tmp https://dlcdn.apache.org/tomcat/tomcat-9/v$tcatnew/bin/apache-tomcat-$tcatnew.tar.gz
