0b2b6a168d45095bc1a81378e44b2c9b9c17d314
Wade Gibson <86313939+wadegib@users.noreply.github.com>
2026-08-30 10:32:29 -0500

    Add script for upgrading YOURLS to version 1.10.6

---
+++ yourls-upgrade.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Execute via "sudo curl https://raw.githubusercontent.com/esu11/misc/refs/heads/main/yourls-upgrade.sh | sudo bash"
+
+# Set variables
+yourlsnew=1.10.6
+
+wget -P /tmp https://github.com/YOURLS/YOURLS/archive/refs/tags/$yourlsnew.tar.gz
+tar xzvf /tmp/$yourlsnew.tar.gz
+sudo cp -R /tmp/YOURLS-$yourlsnew/* /var/www/esu11.info/
+sudo chown -R www-data:www-data /var/www/esu11.info
+sudo cp -R /tmp/YOURLS-$yourlsnew/* /var/www/sveagles.info/
+sudo chown -R www-data:www-data /var/www/sveagles.info
+
+sudo rm /tmp/$yourlsnew.tar.gz
+sudo rm -rf /tmp/YOURLS-$yourlsnew
